d5b922918f06188bcdac8191cd8abb487ba752cb
Stackbit Code Editor <projects@stackbit.com>
2021-10-27 19:01:08 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-				festive: ['"Festive"', 'sans-serif']
+				festive: ['"nto"', 'sans-serif']
 			},
             colors: {
                 body: themeStyle.body,
